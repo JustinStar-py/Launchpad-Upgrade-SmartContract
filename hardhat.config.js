9f0c981c4a585d920bfd0d6c7ea09bcb37047092
JustinStar-py <JustinStar.py@gmail.com>
2023-07-14 15:47:14 +0330

    new solidity files added

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,39 +1,38 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config()
 
-// hardhat.config.js
-// const { mnemonic, bscscanApiKey } = require('./secrets.json');
-
-require('@nomiclabs/hardhat-ethers');
-require("@nomiclabs/hardhat-etherscan");
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
+/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   defaultNetwork: "testnet",
   networks: {
     localhost: {
-      url: "http://127.0.0.1:8545"
+        url: "http://127.0.0.1:8545",
+        accounts: [process.env.LOCALHOST_ACC,]
       },
       hardhat: {
       },
       testnet: {
-        url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+        url: "https://clean-restless-feather.bsc-testnet.discover.quiknode.pro/395608a8a3773bdc951423350b76ea030cc43cae/",
         chainId: 97,
         gasPrice: 20000000000,
-        accounts: ['Your Private-Key',]
+        accounts: [process.env.BSC_ACC,]
       },
       mainnet: {
-        url: "https://bsc-dataseed.binance.org/",
-        chainId: 56,
-        gasPrice: 20000000000,
-        accounts: ['Your Private-Key',]
+          url: "https://bsc-dataseed.binance.org/",
+          chainId: 56,
+          gasPrice: 20000000000,
+          accounts: [process.env.BSC_ACC,]
+      },
+      ethereum: {
+        url: "https://www.noderpc.xyz/rpc-mainnet/public",
+        chainId: 1,
+        accounts: [process.env.ETH_ACC,]
       }
   },
-
   etherscan: {
     // Your API key for Etherscan
     // Obtain one at https://bscscan.com/
-    apiKey: 'AP KEY'
+    apiKey: process.env.ETHERSCAN_API
   },
   solidity: {
     version: "0.8.17",
